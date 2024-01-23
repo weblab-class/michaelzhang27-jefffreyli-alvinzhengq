570fb71016ceceaece395fd793330526051d378e
Michael Zhang <michaelzhang@dhcp-10-29-130-134.dyn.MIT.EDU>
2024-01-22 22:33:20 -0500

    michael changes

---
+++ components/data.js
@@ -0,0 +1,62 @@
+export const data = [
+  {
+    id: 1,
+    name: "Samaria",
+    length: 7,
+    flex: true,
+  },
+  {
+    id: 2,
+    name: "Gauthier",
+    length: 8,
+    flex: true,
+  },
+  {
+    id: 3,
+    name: "Mellisa",
+    length: 3,
+    flex: false,
+  },
+  {
+    id: 4,
+    name: "Arabela",
+    length: 6,
+    flex: true,
+  },
+  {
+    id: 5,
+    name: "Devon",
+    length: 5,
+    flex: false,
+  },
+  {
+    id: 6,
+    name: "Stacee",
+    length: 5,
+    flex: false,
+  },
+  {
+    id: 7,
+    name: "Federica",
+    length: 3,
+    flex: false,
+  },
+  {
+    id: 8,
+    name: "Jecho",
+    length: 5,
+    flex: true,
+  },
+  {
+    id: 9,
+    name: "Alasteir",
+    length: 9,
+    flex: true,
+  },
+  {
+    id: 10,
+    name: "Elston",
+    length: 5,
+    flex: false,
+  },
+];
