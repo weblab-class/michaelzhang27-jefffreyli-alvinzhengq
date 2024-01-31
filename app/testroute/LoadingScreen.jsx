3561678b3e29f3d283a01c659fbe5c5a0151e88e
jeffcoach <156025490+jeffcoach@users.noreply.github.com>
2024-01-30 23:24:56 -0500

    details, loading screen, error msg

--- app/testroute/LoadingScreen.jsx
+++ app/testroute/LoadingScreen.jsx
@@ -5,16 +5,18 @@ const LoadingScreen = ({ subtitle }) => {
   return (
     <div className="h-screen flex justify-center items-center bg-dawn">
       <div>
-        <ColorRing
-          visible={true}
-          height="120"
-          width="120"
-          ariaLabel="color-ring-loading"
-          wrapperStyle={{}}
-          wrapperClass="color-ring-wrapper"
-          colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
-        />
-        <p className="-translate-x-2">{subtitle}</p>
+        <div className="flex justify-center items-center">
+          <ColorRing
+            visible={true}
+            height="120"
+            width="120"
+            ariaLabel="color-ring-loading"
+            wrapperStyle={{}}
+            wrapperClass="color-ring-wrapper"
+            colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+          />
+        </div>
+        <p className="translate-x-2">{subtitle}</p>
       </div>
     </div>
   );
